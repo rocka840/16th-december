b286c228a6474908e8db0ee80801eb1b78155356
unknown <RocKa840@ltpemdomain.local>
2019-12-16 09:37:23 +0100

    hhzzh

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+"use strict";
+
 function changeClock() {
     let divElement = document.getElementById("myClock"); //get the div element, html element
     let secondsCount = Number(divElement.innerHTML); //get the html contents and transform into a number
     secondsCount--; //decrease the seconds
-    divElement.innerHTML = secondsCount; //
+    divElement.innerHTML = secondsCount; //puts the values into the clock html
+    if (secondsCount > 55) {
+
+    };
+    setTimeout(changeClock, 1000);
 }
 
 
